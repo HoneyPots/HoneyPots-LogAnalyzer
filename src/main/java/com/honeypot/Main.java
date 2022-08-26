21b0ecc7ae9f76022ca035465e62535e52b36254
Dokyeongyun <aservmz@naver.com>
2022-08-26 14:50:56 +0900

    Set @EnableBatchProcessing and @SpringBootApplication annotation to Main class

--- src/main/java/com/honeypot/Main.java
+++ src/main/java/com/honeypot/Main.java
@@ -1,7 +1,24 @@
 package com.honeypot;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@EnableBatchProcessing
+@SpringBootApplication
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+        ApplicationContext context = run(Main.class, args);
+        JobLauncher jobLauncher = context.getBean(JobLauncher.class);
+        jobLauncher.run(context.getBean(Job.class), new JobParametersBuilder().toJobParameters());
     }
 }
