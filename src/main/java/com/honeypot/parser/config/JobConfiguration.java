d44316fc9691f0dee24ba2fe4e1af5bca61e76ea
Dokyeongyun <aservmz@naver.com>
2022-08-27 22:21:53 +0900

    Modify JobConfiguration to use configured all step

--- src/main/java/com/honeypot/parser/config/JobConfiguration.java
+++ src/main/java/com/honeypot/parser/config/JobConfiguration.java
@@ -1,32 +1,33 @@
 package com.honeypot.parser.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableBatchProcessing
+@RequiredArgsConstructor
 public class JobConfiguration {
 
-	@Autowired
-	private JobBuilderFactory jobBuilderFactory;
+    private final JobBuilderFactory jobBuilderFactory;
 
-	@Autowired
-	@Qualifier("ansiEscapeCodeRemoveStep")
-	private Step ansiEscapeCodeRemoveStep;
+    private final Step ansiEscapeCodeRemoveStep;
 
-	@Bean
-	public Job processLogFileJob() {
-		return jobBuilderFactory.get("processLogFileJob")
-				.incrementer(new RunIdIncrementer())
-				.flow(ansiEscapeCodeRemoveStep)
-				.end()
-				.build();
-	}
+    private final Step regexNotMatchLineRemoveStep;
+
+    private final Step importLogFileStep;
+
+    @Bean
+    public Job processLogFileJob() {
+        return jobBuilderFactory.get("processLogFileJob")
+                .incrementer(new RunIdIncrementer())
+				.start(ansiEscapeCodeRemoveStep)
+                .next(regexNotMatchLineRemoveStep)
+                .next(importLogFileStep)
+                .build();
+    }
+
 }
